16e6dea21ce4fe707744be98de630c9797e5fc80
Gabriel Moreira <gsm.br2003@gmail.com>
2025-06-04 15:50:24 -0300

    HttpAdapter throws ServerError if throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -45,6 +45,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -131,6 +135,14 @@ void main() {
 
     test('Should return ServerError if returns 500', () async {
       mockResponse(500);
+
+      final response = sut.request(url: url, method: 'post');
+
+      expect(response, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if thorws', () async {
+      mockError();
 
       final response = sut.request(url: url, method: 'post');
 
