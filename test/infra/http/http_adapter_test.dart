dab3ccff7bf30e2fe93dcc53295788fb87ea41e2
Gabriel Moreira <gsm.br2003@gmail.com>
2025-06-03 23:21:01 -0300

    test: HttpAdapter calls post with correct url

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,11 +1,41 @@
-
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import 'http_adapter_test.mocks.dart';
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    required String url,
+    required String method,
+  }) async {
+    await client.post(toUri(url));
+  }
+
+  Uri toUri(String url) => Uri.parse(url);
+}
+
+@GenerateMocks([Client])
 void main() {
   
   group('post', () {
-    test('', () {
-      
+    test('Should call post with correct values', () async {
+      final client = MockClient();
+      final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
+
+      when(client.post(sut.toUri(url))) 
+        .thenAnswer((_) async => Response('', 200));
+
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(sut.toUri(url)));
     });
   });
 }
