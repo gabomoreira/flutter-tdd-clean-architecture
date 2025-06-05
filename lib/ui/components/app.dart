acc0c595cfc01ba06fddf392c8fad8e187623ae7
Gabriel Moreira <gsm.br2003@gmail.com>
2025-06-05 00:12:17 -0300

    feat: add login page layout

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -6,9 +6,52 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final primaryColor = const Color(0xFF005C63);
+    final primaryColorDark = const Color(0xFF004349);
+    final primaryColorLight = const Color(0xFF4FBDBA);
+    final backgroundColor = const Color(0xFFF6F6F6);
+    final textColor = const Color(0xFF1C1C1C);
+
+
     return MaterialApp(
       title: "gabo's",
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        primaryColorDark: primaryColorDark,
+        primaryColorLight: primaryColorLight,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: primaryColor
+        ),
+        scaffoldBackgroundColor: backgroundColor,
+        textTheme: TextTheme(
+          headlineLarge: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: primaryColorDark
+          )
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColorLight),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColor),
+          ),
+          alignLabelWithHint: true
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            foregroundColor: Colors.white,
+            backgroundColor: primaryColor,
+            overlayColor: primaryColorLight,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
+            )
+          )
+        )
+      ),
       home: LoginPage(),
     );
   }
