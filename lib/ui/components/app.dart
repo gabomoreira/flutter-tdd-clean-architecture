8bf795b7e0b0c3c71488552975e119432d2f8a5d
Gabriel Moreira <gsm.br2003@gmail.com>
2025-06-04 23:00:03 -0300

    feat: add LoginPage

---
+++ lib/ui/components/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../../ui/pages/pages.dart';
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "gabo's",
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
